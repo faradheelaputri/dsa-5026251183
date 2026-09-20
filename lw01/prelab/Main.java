12b298951d84032d3a795223ef70c1d1bb645860
faradheelaputri <faradheelaminaa@gmail.com>
2026-09-21 06:48:29 +0700

    Labwork 01 Prelab

---
+++ lw01/prelab/Main.java
@@ -0,0 +1,48 @@
+package lw01.prelab;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<PrintJob> jobs = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File("lw01/prelab/jobs.txt"))) {
+            while (scanner.hasNext()) {
+                String type = scanner.next();
+                String id = scanner.next();
+                int pages = scanner.nextInt();
+
+                if (type.equalsIgnoreCase("MONO")) {
+                    jobs.add(new MonoPrint(id, pages));
+                } else if (type.equalsIgnoreCase("COLOUR")) {
+                    jobs.add(new ColourPrint(id, pages));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            try (Scanner scanner = new Scanner(new File("jobs.txt"))) {
+                while (scanner.hasNext()) {
+                    String type = scanner.next();
+                    String id = scanner.next();
+                    int pages = scanner.nextInt();
+
+                    if (type.equalsIgnoreCase("MONO")) {
+                        jobs.add(new MonoPrint(id, pages));
+                    } else if (type.equalsIgnoreCase("COLOUR")) {
+                        jobs.add(new ColourPrint(id, pages));
+                    }
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("File jobs.txt tidak ditemukan!");
+                return;
+            }
+        }
+
+        for (PrintJob job : jobs) {
+            System.out.println(job.summary());
+        }
+    }
+}
